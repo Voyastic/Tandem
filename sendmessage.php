6951ce784c1ada4fa1f237f092c379997fce0efe
Voyastic <ameneyro@voyastic.com>
2014-06-24 13:52:25 -0500

    z-index

--- sendmessage.php
+++ sendmessage.php
@@ -4,7 +4,7 @@ $usermail = $_POST['email'];
 $name = $_POST['name'];
 $content  = nl2br($_POST['msg']);
 
-$subject  = "New Feedback Message";
+$subject  = "Nuevo mensaje";
 $headers  = "From: " . strip_tags($usermail) . "\r\n";
 $headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
@@ -12,13 +12,13 @@ $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
 $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Nuevo mensaje</h2>\r\n";
-$msg .= "<p><strong>Sent by:</strong> ".$usermail."</p>\r\n";
-$msg .= "<p><strong>Sent by:</strong> ".$name."</p>\r\n";
-$msg .= "<p><strong>Message:</strong> ".$content."</p>\r\n";
+$msg .= "<p><strong>Correo:</strong> ".$usermail."</p>\r\n";
+$msg .= "<p><strong>Nombre:</strong> ".$name."</p>\r\n";
+$msg .= "<p><strong>Mensaje:</strong> ".$content."</p>\r\n";
 $msg .= "</body></html>";
 
 
-if(@mail($sendto, $subject, $msg, $headers)) {
+if(@mail($sendto, $subject, $msg,$name, $headers)) {
 	echo "true";
 } else {
 	echo "false";
