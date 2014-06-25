4838ada8903f6dc04dfe2864dc779d9b4e1e5e8e
Voyastic <ameneyro@voyastic.com>
2014-06-25 12:57:14 -0500

    lenguajes

--- indexes.php
+++ indexes.php
@@ -13,10 +13,17 @@
 <link rel="stylesheet" type="text/css" href="css/layout.css">
 <link rel="stylesheet" type="text/css" href="css/skeleton.css">
 <link rel="stylesheet" type="text/css" href="fonts/fonts.css">
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+  <link rel="stylesheet" type="text/css" media="all" href="style.css">
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery.fancybox.js?v=2.0.6"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+<script src="js/jquery.slicknav.js"></script>
+
 <script type="text/javascript">
-	$(document).ready(function(){
+    $(document).ready(function(){
     $('section[data-type="background"]').each(function(){
         var $bgobj = $(this); // assigning the object
      
@@ -31,25 +38,12 @@
         }); 
     });    
 });
-	// Create HTML5 elements for IE
+    // Create HTML5 elements for IE
   
 document.createElement("article");
 document.createElement("section");
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
     $('.menu a').each(function () {
@@ -66,7 +60,7 @@ function onScroll(event){
 }
 $(document).scroll(function () {
     var y = $(this).scrollTop();
-    if (y > 600) {
+    if (y > 360) {
         $('.fade').fadeIn(1000);
     } else {
         $('.fade').fadeOut();
@@ -79,25 +73,31 @@ $(document).scroll(function () {
     }
 });
 </script>
-
 </head>
 <body>
 <section id="home" data-type="background" data-speed="10">
 <div class="container">
 	<article class="three columns logo"><img src="images/logotipo.png" alt="logotipo Tandem"></article>
-    <article class="twelve columns menu">
-        <nav>            
-            <a href="index.html#home"><li>Home</li></a>
-            <a href="index.html#about"><li>Nosotros</li></a>
-            <a href="index.html#services"><li>Servicios</li></a>
-            <a href="index.html#abogados"><li>Abogados</li></a>
-            <a href="index.html#publicaciones"><li>Publicaciones</li></a>
-            <a href=""><li>Contacto</li></a>
-        </nav>
+    <article class="twelve columns">
+        <ul id="menu">            
+            <a href="index.php#home"><li>Home</li></a>
+            <a href="index.php#about"><li>Nosotros</li></a>
+            <a href="index.php#services"><li>Áreas</li></a>
+            <a href="index.php#abogados"><li>Abogados</li></a>
+            <a href="index.php#publicaciones"><li>Publicaciones</li></a>
+            <a class="modalbox" href="#inline"><li>Contacto</li></a>
+
+        </ul>
+<script type="text/javascript">
+$(document).ready(function(){
+    $('#menu').slicknav();
+});
+</script>
     </article>
 </div>  
 <div class="container frase">
-    <h1>CONTAMOS CON UN POOL DE ABOGADOS ASOCIADOS ALTAMENTE ESPECIALIZADOS EN DISTINTAS ÁREAS DEL DERECHO.</h1>
+    <h1>ABOGADOS ASOCIADOS ALTAMENTE CAPACITADOS PARA
+ATENDERLE EN DIVERSAS ÁREAS DEL DERECHO, HACEMOS TANDEM CON USTED</h1>
 </div>
 </section>   
      
@@ -146,78 +146,72 @@ Buscamos el beneficio de entidades y organismos públicos ofreciendo  la elabora
 <section id="serv" data-type="background" data-speed="10">
 <div class="container">
     <article class="four columns">
-        <h3>DERECHO CORPORATIVO.</h3>
+        <h3>CORPORATIVO.</h3>
         <ul>
-            <li>Creación de empresas</li>
-            <li>Actas de asambleas</li>
-            <li>Nulidad de asambleas</li>
-            <li>Admisión y expulsión de socios</li>
-            <li>Protocolizaciones</li>
-            <li>Emisión de acciones</li>
-            <li>Libros corporativos</li>
-            <li>Capitalizaciones</li>
-            <li>Escisión y fusión de empresas y reestructuraciones.</li>
+            <li>Creación, restructuración y terminación de sociedades</li>
+            <li>Actas y documentación para asambleas</li> 
+            <li>Protocolizaciones y registro</li>
+            <li> Libros corporativos</li>
+            <li>Modificaciones de capital</li>
+            <li>Auditoría legal</li>
+           
         </ul>
     </article>
  <article class="four columns">
-        <h3>DERECHO CIVIL Y MERCANTIL.</h3>
+        <h3>MERCANTIL Y CIVIL.</h3>
         <ul>
-            <li>Contratos</li>
-<li>Convenios</li>
-<li>Asuntos hipotecarios</li>
-<li>Seguros y fianzas</li>
-<li>Juicios mercantiles y civiles</li>
+            <li> Contratos y convenios en general</li>
+<li>Garantías (hipoteca, prenda)</li>
+<li>Fianzas</li>
+<li>Fideicomisos</li>
  
         </ul>
     </article>
         <article class="four columns">
-        <h3>DERECHO FISCAL Y ADMINISTRATIVO.
-</h3>
+        <h3>FISCAL Y ADMINISTRATIVO.</h3>
         <ul>
-            <li>Creación de empresas</li>
-            <li>Actas de asambleas</li>
-            <li>Nulidad de asambleas</li>
-            <li>Admisión y expulsión de socios</li>
-            <li>Protocolizaciones</li>
-            <li>Emisión de acciones</li>
-            <li>Libros corporativos</li>
-            <li>Capitalizaciones</li>
-            <li>Escisión y fusión de empresas y reestructuraciones.</li>
+            <li>Procedimientos ante autoridades gubernamentales (locales, estatales, federales)</li>
+            <li>Licitación y compras públicas</li>
+            <li> Verificaciones e inspecciones</li>
+            <li>Recursos y juicios contenciosos</li>
+            <li> Seguridad social (IMSS, INFONAVIT)</li>
+            <li>Hacienda</li>   
         </ul>
     </article>
      <article class="four columns">
-        <h3>MARCAS Y PATENTES.</h3>
+        <h3>PROPIEDAD INDUSTRIAL E INTELECTUAL.</h3>
         <ul>
-            <li>Patentes</li>
-<li>Marcas</li>
-<li>Derechos de autor</li>
-<li>Trámite de renovación de registros y excesiones de derechos.</li>
-            
+            <li>Registro de marcas, patentes y derechos de autor</li>
+            <li>Procedimiento administrativo</li>
+            <li>Cesiones</li>
+            <li>Franquicias</li>
+            <li>Recursos y juicios contenciosos</li>   
         </ul>
     </article>
 </div>
 <br>
 <div class="container">
     <article class="four columns">
-        <h3>DERECHO LABORAL.</h3>
+        <h3>LABORAL.</h3>
         <ul>
-            <li>Relaciones de trabajo: contratación 
- y terminación.</li>
-<li>Procedimientos administrativos y
-seguridad social.</li>
-
+            <li>Contratos de trabajo</li>
+            <li>Actas administrativas</li>
+            <li>Terminación de relaciones laborales</li>
+            <li>Contratación colectiva</li>
+            <li>Elaboración de documentos (reglamentos, comités)</li>
+            <li>Procedimientos administrativos y de seguridad social</li>
             
         </ul>
     </article>
     <article class="four columns">
-         <h3>LITIGIO Y 
-AMPAROS.</h3>
+         <h3>CONTRATOS.</h3>
         <ul>
-            <li>En materia fiscal, administrativos, fiscal y civil.
-</li>
+            <li> Civiles (arrendamientos, donaciones, comodatos)</li>
+            <li>Mercantiles (compraventas, asociaiones en participación,joint ventures)</li>
+            <li>Laborales (contratos individuales y colectivos)</li>
     </article>
     <article class="eight columns">
-         <h3>DERECHO LABORAL.</h3>
+         <h3>LITIGIO.</h3>
         <ul>
             <li>Civiles (arrendamiento, donaciones, comodatos, etc.)</li>
 <li>Mercantiles (compraventas, asociaciones en participación, joint ventures, etc.) </li>
@@ -232,13 +226,16 @@ AMPAROS.</h3>
 
 <h2>ABOGADOS</h2></div>
 <div class="seven columns">
-<p>Contamos con los mejores abogados asociados altamente calificados con una amplia experiencia y conocimiento de las leyes y regulaciones, respondiendo con excelencia a las necesidades de los clientes. </div>
-</p></article>
+<p>Contamos con los mejores abogados asociados altamente
+calificados con una amplia experiencia y conocimiento de la
+normatividad, respondiendo con excelencia a las necesidades de los
+clientes.</p> </div>
+</article>
 <article class="container">
   <div class="flip"> 
         <div class="card"> 
             <div class="face front"> 
-                <img src="images/profile.png">
+             <img src="images/ar.png">
                 <h5>ARMANDO ROBLEDO MÁRQUEZ</h5>
                 <p>Abogado, académico y socio fundador de TANDEM CONSULTORES LEGALES, S.C.  </p>
                 <ul>
@@ -251,7 +248,9 @@ AMPAROS.</h3>
             <div class="face back"> 
                                 <h5>ARMANDO ROBLEDO MÁRQUEZ</h5>
 
-                <p>Su área de especialización es el Derecho administrativo y fiscal.  Ha ejercido como abogado litigante en las áreas del Derecho mercantil y tributario; además, trabajo para el Servicio de Administración Tributaria de la SHCP (1997-1999) y fungió como Director General Jurídico de la Secretaría de Finanzas del Gobierno estatal (2004-2010), además de haber formado parte del Consejo de la Judicatura estatal (2009-2010).</p>
+                <p>Su área de especialización es el Derecho administrativo y fiscal.  Ha ejercido como abogado litigante en las áreas del Derecho mercantil y 
+                    tributario; además, trabajo para el Servicio de Administración Tributaria de la SHCP (1997-1999) y fungió como Director General Jurídico de
+                     la Secretaría de Finanzas del Gobierno estatal (2004-2010)</p>
  <p>
 En la academia, ha impartido distintos cursos en materia tributaria y administrativa, tanto en cursos a nivel de licenciatura como en postgrados y foros especializados.  </p>
  <p>
@@ -262,7 +261,7 @@ Actualmente es profesor de la materia de Derecho fiscal en la Licenciatura en De
       <div class="flip"> 
         <div class="card"> 
             <div class="face front"> 
-              <img src="images/profile.png">
+           <img src="images/man.png">
                 <h5>JOSÉ MANUEL LÓPEZ LIBREROS</h5>
                 <p>Abogado, académico y socio fundador de TANDEM CONSULTORES LEGALES, S.C.  </p>
                 <ul>
@@ -270,13 +269,20 @@ Actualmente es profesor de la materia de Derecho fiscal en la Licenciatura en De
   <li>Master en Derecho de la Unión Europea (2002) </li>
   <li>Doctor en Derecho por la Universidad Carlos III de Madrid (2008).  
  </li>
+ <li>Formó parte del Consejo de la Judicatura estatal (2009-2010).</li>
                 </ul>
             </div> 
             <div class="face back"> 
                                 <h5>JOSÉ MANUEL LÓPEZ LIBREROS</h5>
 
-                <p>Derecho mercantil, corporativo, administrativo e internacional. Se ha desempeñado como abogado litigante en las áreas del Derecho privado, además de haber sido Coordinador jurídico del Gobierno estatal (2008-2010).En el área académica, ha dado cursos en postgrados relacionados con el área del Derecho corporativo, administrativo, derechos humanos y derecho internacional; y actualmente es investigador por parte de la Universidad Autónoma de Aguascalientes.</p>
-<p>Cuenta con la publicación de diversos artículos en relación al comercio de mercancías y servicios en los sistemas jurídico-comerciales del TLCAN, la UE y la OMC; así como en materia de derecho internacional y los derechos humanos.  Actualmente es profesor de la materia Sistemas Jurídicos Contemporáneos y Derecho internacional público en la Licenciatura en Derecho de la Universidad Autónoma de Aguascalientes; además de dar cursos en otras instituciones públicas y privadas de la región.  Domina de manera fluida el idioma inglés, y además, maneja el idioma francés.</p>
+                <p>Derecho mercantil, corporativo, administrativo e internacional. Se ha desempeñado como abogado litigante en las áreas del Derecho privado, 
+                    además de haber sido Coordinador jurídico del Gobierno estatal (2008-2010).En el área académica, ha dado cursos en postgrados relacionados 
+                    on el área del Derecho corporativo, administrativo, derechos humanos y derecho internacional; y actualmente es investigador por parte de la
+                     Universidad Autónoma de Aguascalientes.</p>
+<p>Cuenta con la publicación de diversos artículos en relación al comercio de mercancías y servicios en los sistemas jurídico-comerciales del TLCAN, 
+    la UE y la OMC; así como en materia de derecho internacional y los derechos humanos.  Actualmente es profesor de la materia Sistemas Jurídicos 
+    Contemporáneos y Derecho internacional público en la Licenciatura en Derecho de la Universidad Autónoma de Aguascalientes; además de dar cursos en 
+    otras instituciones públicas y privadas de la región. Domina de manera fluida el idioma inglés, y además, maneja el idioma francés.</p>
  </p>
  <p>
 Actualmente es profesor de la materia de Derecho fiscal en la Licenciatura en Derecho de la Universidad Autónoma de Aguascalientes.  Cuenta con diversas publicaciones relacionadas con el ámbito fiscal.  Maneja de manera fluida el idioma inglés.</p>
@@ -286,7 +292,7 @@ Actualmente es profesor de la materia de Derecho fiscal en la Licenciatura en De
      <div class="flip"> 
         <div class="card"> 
             <div class="face front"> 
-              <img src="images/profile.png">
+             <img src="images/profileaz.png">
                 <h5>LAURA ELENA LÓPEZ CARMONA
 </h5>
                 <p>Abogada, académica y socia fundadora de TANDEM CONSULTORES LEGALES, S.C.  </p>
@@ -300,7 +306,7 @@ Actualmente es profesor de la materia de Derecho fiscal en la Licenciatura en De
                                 <h5>LAURA ELENA LÓPEZ CARMONA
 </h5>
 
-                <p>Derecho civil, familiar, corporativo y administrativo. Se ha desempeñado como abogada litigante en las áreas del Derecho privado y público, además de haber sido encargada de la Oficina regional 2 de la Comisión de Apelación y Arbitraje del Deporte.</p>
+                <p>Su área de especialización es el derecho civil, familiar, corporativo y administrativo. Se ha desempeñado como abogada litigante en las áreas del Derecho privado y público, además de haber sido encargada de la Oficina regional 2 de la Comisión de Apelación y Arbitraje del Deporte.</p>
 <p>Actualmente funge como catedrática en diversas instituciones locales, en el ámbito del Derecho civil y familiar.
 </p>
             </div> 
@@ -317,14 +323,122 @@ Actualmente es profesor de la materia de Derecho fiscal en la Licenciatura en De
 </article>
 </section>
 
+<div id="inline">
+    <h2>Contáctanos</h2>
+
+    <form id="contact" name="contact" action="#" method="post">
+        <label for="name">Nombre</label>
+        <input type="name" id="name" name="name" class="txt">
+        <label for="email">Correo electrónico</label>
+        <input type="email" id="email" name="email" class="txt">
+        <br>
+        <label for="msg">Mensaje</label>
+        <textarea id="msg" name="msg" class="txtarea"></textarea>
+        
+        <button id="send">Enviar</button>
+    </form>
+</div>
+<script type="text/javascript">
+    function validateEmail(email) { 
+        var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return reg.test(email);
+    }
+
+    $(document).ready(function() {
+        $(".modalbox").fancybox();
+        $("#contact").submit(function() { return false; });
+
+        
+        $("#send").on("click", function(){
+            var emailval  = $("#email").val();
+            var msgval    = $("#msg").val();
+            var msglen    = msgval.length;
+            var mailvalid = validateEmail(emailval);
+            
+            if(mailvalid == false) {
+                $("#email").addClass("error");
+            }
+            else if(mailvalid == true){
+                $("#email").removeClass("error");
+            }
+            
+            if(msglen < 4) {
+                $("#msg").addClass("error");
+            }
+            else if(msglen >= 4){
+                $("#msg").removeClass("error");
+            }
+            
+            if(mailvalid == true && msglen >= 4) {
+                // if both validate we attempt to send the e-mail
+                // first we hide the submit btn so the user doesnt click twice
+                $("#send").replaceWith("<em>Enviando mensaje...</em>");
+                
+                $.ajax({
+                    type: 'POST',
+                    url: 'sendmessage.php',
+                    data: $("#contact").serialize(),
+                    success: function(data) {
+                        if(data == "true") {
+                            $("#contact").fadeOut("fast", function(){
+                                $(this).before("<p><strong>Su mensaje ha sido enviado exitosamente. Gracias por ponerte en contacto con nosotros.</strong></p>");
+                                setTimeout("$.fancybox.close()", 2000);
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    });
+</script>
+
+    </article>
+
+</section>
 <section id="publicaciones">
      <article class="container">
      <div class="sixteen columns"> 
                 <div class="line"></div>
 
 <h2>PUBLICACIONES RECIENTES</h2></div>
+<?php 
+    require($_SERVER['DOCUMENT_ROOT'] . '/publicaciones/wp-load.php'); 
+    $args = array(
+        // 'cat' => 4, // Only source posts from a specific category
+        'posts_per_page' => 3 // Specify how many posts you'd like to display
+    );
+    $latest_posts = new WP_Query( $args );  
+    if ( $latest_posts->have_posts() ) {
+        while ( $latest_posts->have_posts() ) {
+            $latest_posts->the_post(); ?>
+  
+<div class="en style five columns">
+
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+    <?php if ( has_post_thumbnail() ) { ?>
+        <p class="post_thumbnail"><?php the_post_thumbnail(); ?></p>
+    <?php } ?>
+            <h4 class="post_title"><?php the_title(); ?></h4>
+        </a>
+        <p class="post_time">Posted on <?php the_time('l jS F, Y') ?></p>
+        <?php the_excerpt(); ?>
+
+
+    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <button class="ver"> Seguir leyendo..</button> </a></div>
+
+<? } 
+        } else {
+        echo '<h1>There are no posts available</h1>';
+    }
+    wp_reset_postdata();
+?>      
 </article>
 </section>
+
+
+
+
 <footer>
     <section class="container">
         <article class="four columns"><img src="images/logotipoazul.png"></article>
